b635170c664103decd10da367a4901cd405b82fb
Tamás Kacsó Gáspár <tkgaspar@yahoo.de>
2022-04-30 16:32:00 +0300

    extended main from SpringBootServletInitializer

--- src/main/java/com/ownproject/ServiceOrganizer/ServiceOrganizerApplication.java
+++ src/main/java/com/ownproject/ServiceOrganizer/ServiceOrganizerApplication.java
@@ -2,12 +2,14 @@ package com.ownproject.ServiceOrganizer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class ServiceOrganizerApplication {
+public class ServiceOrganizerApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceOrganizerApplication.class, args);
@@ -17,4 +19,11 @@ public class ServiceOrganizerApplication {
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure (SpringApplicationBuilder application) {
+		return application.sources (ServiceOrganizerApplication.class);
+	}
+
+
 }
