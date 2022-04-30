b59d714fd0f48c2cbc0a25cfee9c230ad854044d
Tamás Kacsó Gáspár <tkgaspar@yahoo.de>
2022-05-01 00:06:50 +0300

    green bean III.

--- src/main/java/com/ownproject/ServiceOrganizer/Controller/MyErrorController.java
+++ src/main/java/com/ownproject/ServiceOrganizer/Controller/MyErrorController.java
@@ -4,9 +4,11 @@ import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.DispatcherServlet;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class MyErrorController implements ErrorController {
@@ -17,7 +19,7 @@ public class MyErrorController implements ErrorController {
 
     @RequestMapping("/error")
     public String getErrorPage(HttpServletRequest request, Model model) {
-        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        Object status = request.getAttribute("javax.servlet.error.status_code");
         model.addAttribute("PageNotFoundError", true);
         return "/error";
     }
