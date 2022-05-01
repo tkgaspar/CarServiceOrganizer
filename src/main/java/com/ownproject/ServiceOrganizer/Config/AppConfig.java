33f4092d57e5c21cee168398c7fedf300acbc427
Tamás Kacsó Gáspár <tkgaspar@yahoo.de>
2022-05-01 22:49:03 +0300

    implemented the  WebApplicationInitializer

--- src/main/java/com/ownproject/ServiceOrganizer/Config/AppConfig.java
+++ src/main/java/com/ownproject/ServiceOrganizer/Config/AppConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
-@EnableWebMvc
+
 @Configuration
 
 public class AppConfig {
--- src/main/java/com/ownproject/ServiceOrganizer/Config/SOAppInitializer.java
+++ src/main/java/com/ownproject/ServiceOrganizer/Config/SOAppInitializer.java
@@ -1,13 +1,32 @@
 package com.ownproject.ServiceOrganizer.Config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
 
-public class SOAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
+public class SOAppInitializer implements WebApplicationInitializer {
 
-    @Override
+    public void onStartup(ServletContext container) throws ServletException {
+
+        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+        ctx.register(AppConfig.class);
+        ctx.setServletContext(container);
+
+        ServletRegistration.Dynamic servlet = (ServletRegistration.Dynamic) new DispatcherServlet(ctx);
+
+        servlet.setLoadOnStartup(1);
+        servlet.addMapping("/control");
+
+    }
+
+  /*  @Override
     protected Class<?>[] getRootConfigClasses() {
         return new Class[0];
     }
@@ -24,4 +43,9 @@ public class SOAppInitializer extends AbstractAnnotationConfigDispatcherServletI
 
         return arr;
     }
+
+    @Override
+    protected String getServletName() {
+        return "appservlet";
+    }*/
 }
