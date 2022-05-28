23e255a3dafeeb319603fcffdb6dec57b8731c43
Tamás Kacsó Gáspár <tkgaspar@yahoo.de>
2022-05-28 12:33:32 +0300

    all html content to WEB-INF

--- src/main/java/com/ownproject/ServiceOrganizer/Config/AppConfig.java
+++ src/main/java/com/ownproject/ServiceOrganizer/Config/AppConfig.java
@@ -5,22 +5,29 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @EnableWebMvc
 @ComponentScan(basePackages = "com.ownproject.ServiceOrganizer")
 @Configuration
 
-public class AppConfig {
+public class AppConfig extends WebMvcConfigurerAdapter {
 
     // Resolve logical view names to .html resources in the /WEB-INF/views directory
     @Bean
     ViewResolver viewResolver() {
         InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-        resolver.setPrefix("WEB-INF/classes/templates");
+        resolver.setPrefix("WEB-INF/");
         resolver.setSuffix(".html");
         return resolver;
+    }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
     }
 }
 
